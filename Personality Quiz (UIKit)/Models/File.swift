0697f75729e4b8899934704e1919844eaf91e191
Fedor Boretsky <fedor.boretsky@gmail.com>
2022-02-20 15:28:34 +0300

    Added app icon.

---
+++ Personality Quiz (UIKit)/Models/File.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  Personality Quiz (UIKit)
+//
+//  Created by Fedor Boretskiy on 20.02.2022.
+//
+
+import Foundation
--- Personality Quiz (UIKit)/View Controllers/WelcomeViewController.swift
+++ Personality Quiz (UIKit)/View Controllers/WelcomeViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  WelcomeViewController.swift
 //  Personality Quiz (UIKit)
 //
 //  Created by Fedor Boretskiy on 18.02.2022.
@@ -7,13 +7,15 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class WelcomeViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
+    @IBAction func unwindToWelcomeScreen(_ unwindSegue: UIStoryboardSegue) {
+        
+    }
 
 }
 
