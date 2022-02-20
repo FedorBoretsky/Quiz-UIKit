3a14f33ea79f282e3d8651d405883c6395590139
Fedor Boretsky <fedor.boretsky@gmail.com>
2022-02-20 19:58:10 +0300

    Added quiestion's list.

---
+++ Personality Quiz (UIKit)/Data/Quiestion+List.swift
@@ -0,0 +1,29 @@
+//
+//  Quiestion+List.swift
+//  Personality Quiz (UIKit)
+//
+//  Created by Fedor Boretskiy on 20.02.2022.
+//
+
+extension Question {
+    static let list: [Question] = [
+        Question(text: "Какую еду вы предпочитаете?", responseType: .singleChoice, answers: [
+            Answer(text: "Стейк", animalType: .dog),
+            Answer(text: "Рыбу", animalType: .cat),
+            Answer(text: "Морковку", animalType: .rabbit),
+            Answer(text: "Кукурузу", animalType: .turtle),
+        ]),
+        Question(text: "Что вы любите делать?", responseType: .multipleChoice, answers: [
+            Answer(text: "Плавать", animalType: .turtle),
+            Answer(text: "Спать", animalType: .cat),
+            Answer(text: "Обниматься", animalType: .rabbit),
+            Answer(text: "Есть", animalType: .dog),
+        ]),
+        Question(text: "Любите ли вы поездки на машине?", responseType: .range, answers: [
+            Answer(text: "Ненавижу", animalType: .cat),
+            Answer(text: "Нервничаю", animalType: .rabbit),
+            Answer(text: "Не замечаю", animalType: .turtle),
+            Answer(text: "Обожаю", animalType: .dog),
+        ]),
+    ]
+}
